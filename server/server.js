68647f236fa1d6755806ea046e9f83b8590f15b0
Abhay <abhayrao2k@gmail.com>
2024-12-22 21:10:10 +0530

    feature: to check if user exists in db

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import connect from "./db/connect.js";
 import fs from "fs";
+import User from "./models/userModel.js";
+import asyncHandler from "express-async-handler";
 dotenv.config();
 
 const app = express();
@@ -19,34 +21,72 @@ const config = {
   issuerBaseURL: process.env.ISSUER_BASE_URL,
 };
 
-app.use(cors(
-  {
+app.use(
+  cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
-  }
-));
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(auth(config));
 
-// function to check if user exists in database
+// function to check if user exists in db
+const ensureUserInDB = asyncHandler(async(user)=> {
+  try {
+    const existingUser = await User.findOne({auth0Id: user.sub});
+
+    if(!existingUser){
+      //create a new user
+
+      const newUser = new User({
+        auth0Id: user.sub,
+        email: user.email,
+        name: user.name,
+        role: "jobseeker",
+        profilePicture: user.picture,
+      })
+
+      await newUser.save();
+
+      console.log("User added to db", user)
+    } else {
+      console.log("User already exists", existingUser)
+    }
+  } catch (error) {
+    console.log("Error checking or adding user to db", error.message)
+  }
+})
+
+app.get("/", async(req, res) => {
+  if (req.oidc.isAuthenticated()){
+    // to check if user exists in db
+    await ensureUserInDB(req.oidc.user);
+
+    // redirect to the frontend
+    return res.redirect(process.env.CLIENT_URL)
+  } else {
+    return res.send("Logged out")
+  }
+})
 
 // routes
 const routeFiles = fs.readdirSync("./routes");
 
-routeFiles.forEach((file)=> {
+routeFiles.forEach((file) => {
   // import dynamic routes
-  import(`./routes/${file}`).then((route)=> {
-    app.use("/api/v1/", route.default);
-  })
-  .catch((error)=> {
-    console.log("Error importing route", error);
-  })
-})
+  import(`./routes/${file}`)
+    .then((route) => {
+      app.use("/api/v1/", route.default);
+    })
+    .catch((error) => {
+      console.log("Error importing route", error);
+    });
+});
 
-const server = async() => {
+const server = async () => {
   try {
     await connect();
     app.listen(process.env.PORT, () => {
@@ -56,6 +96,6 @@ const server = async() => {
     console.log("Server error", error.message);
     process.exit(1);
   }
-}
+};
 
 server();
